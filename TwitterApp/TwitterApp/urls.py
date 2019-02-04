4c83b1b8662dab1be0de35713639936bb6703922
Максим Литвинчук <max22111@MacBook-Pro-Maksim.local>
2019-02-04 16:33:15 +0200

    Create admin user

--- TwitterApp/TwitterApp/urls.py
+++ TwitterApp/TwitterApp/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('register/',user_views.register,name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='usr/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='usr/logout.html'), name='logout'),
-    path('profile',user_views.profile,name='profile'),
+    path('profile/',user_views.profile,name='profile'),
     path('',include('blog.urls')),
 ]\
             + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
